fb59d82febf7e3fcf4750dc8484b42754a7df746
lucyTrump <lucytump@dtx.trade>
2024-05-30 20:17:30 +0800

    feat: code init

---
+++ src/constants.ts
@@ -0,0 +1,24 @@
+import { Percent, V2_FACTORY_ADDRESSES } from '@uniswap/sdk-core'
+import JSBI from 'jsbi'
+
+/**
+ * @deprecated use FACTORY_ADDRESS_MAP instead
+ */
+export const FACTORY_ADDRESS = '0x13Cf71Ec4Aa04E359Aad60ff795ec7312A190C85'
+
+export const FACTORY_ADDRESS_MAP: { [chainId: number]: string } = V2_FACTORY_ADDRESSES
+
+export const INIT_CODE_HASH = '0x8615843ab28b4b86b2382dca22cf14f0a6ba9e52cb006531eb574042a5b54a46'
+
+export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
+
+// exports for internal consumption
+export const ZERO = JSBI.BigInt(0)
+export const ONE = JSBI.BigInt(1)
+export const FIVE = JSBI.BigInt(5)
+export const _997 = JSBI.BigInt(997)
+export const _1000 = JSBI.BigInt(1000)
+export const BASIS_POINTS = JSBI.BigInt(10000)
+
+export const ZERO_PERCENT = new Percent(ZERO)
+export const ONE_HUNDRED_PERCENT = new Percent(ONE)
